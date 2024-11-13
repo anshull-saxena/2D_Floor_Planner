23840faa5f62851ba17c0ecfa1c23311b0e185b0
Anshul Saxena <f20221041@hyderabad.bits-pilani.ac.in>
2024-11-13 22:00:42 +0530

    changes

--- src/main/java/Door.java
+++
@@ -1,9 +0,0 @@
-public class Door {
-public int ht,wt,x,y;
-public Door(int width, int height,int x,int y) {
-        this.x = x;
-        this.y = y;
-        this.wt = width;
-        this.ht = height;
-    }
-}
